f1c3c64029bfb70848b908a1b8dce0f036ec67c8
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-06-06 15:09:34 +0300

    max int edit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    biggest = 0
-    for index in my_list:
-        if index > biggest:
-            biggest = index
-    return biggest
+    maxint = sorted(my_list, reverse=True)
+    return maxint[0]
