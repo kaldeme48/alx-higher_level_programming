bb60da3c4c0937a406a70b2b6bf15736dd1b68e9
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-06-06 15:16:31 +0300

    adv

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
+		printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
