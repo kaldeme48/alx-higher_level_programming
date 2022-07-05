75b582fdc04deed991e2f7e2c376abea54b1a65e
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-07-05 13:59:15 +0300

    input output commit correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,7 @@ Adds all arguments to a Python list,
 and then save them to a file.
 """
 
-import json
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
